276d5fcda5db1e009de7aab6ced108025b553581
Quentin <quentin.mang@etu.univ-lyon1.fr>
2021-04-19 10:12:31 +0200

    Update q2
    Q1 faite et q2 en cours

--- src/ChargementData.java
+++ src/ChargementData.java
@@ -12,17 +12,16 @@ public class ChargementData {
             File myObj = new File(filePath);
             Scanner myReader = new Scanner(myObj);
             int i = 0;
+            String data = myReader.nextLine();
+            String[] line = data.split(" ");
+            dataSet.setTailleBin(Integer.parseInt(line[0]));
+            dataSet.setNbItems(Integer.parseInt(line[1]));
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                String[] line = data.split(" ");
-                if (line.length == 1) {
-                    Items item = new Items(Integer.parseInt(line[0]), i);
-                    dataSet.addItems(item);
-                    i++;
-                }else{
-                    dataSet.setTailleBin(Integer.parseInt(line[0]));
-                    dataSet.setNbItems(Integer.parseInt(line[1]));
-                }
+                String data2 = myReader.nextLine();
+                String[] line2 = data2.split(" ");
+                Items item = new Items(Integer.parseInt(line2[0]), i);
+                dataSet.addItems(item);
+                i++;
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -31,6 +30,4 @@ public class ChargementData {
         }
         return dataSet;
     }
-
-    public DataSet
 }
--- src/ListerRepertoire.java
+++ src/ListerRepertoire.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ListerRepertoire {
+import java.io.*;
+
+public class ListerRepertoire {
+
+    public String[] ListerFichier(String path){
+        File repertoire = new File(path);
+        String liste[] = repertoire.list();
+
+        if (liste != null) {
+            for (int i = 0; i < liste.length-1; i++) {
+                System.out.println(liste[i]);
+            }
+        } else {
+            System.err.println("Nom de repertoire invalide");
+        }
+        return liste;
+    }
 }
